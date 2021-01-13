93c4ca4955279d68b1fb7ef97286948861508550
Sarah Chen <sarah.chen6@gmail.com>
2021-01-12 20:25:42 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup, find_packages 
 
-# with open('requirements.txt') as f:
-#    requirements = f.readlines() 
-
 long_description = '''insert \ 
     long \
     description''' 
