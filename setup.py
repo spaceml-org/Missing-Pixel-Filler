c63f99acda5d9e72148aa76be7d81cb26283ce91
Sarah Chen <sarah.chen6@gmail.com>
2021-01-12 20:46:55 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import setup, find_packages 
 
-long_description = '''insert \ 
-    long \
-    description''' 
+long_description = '''Python package used to fill \ 
+    an image's missing data gaps. \
+    Specifically useful for obscuring \
+    image patterns in a machine \
+    learning context.''' 
 
 setup(
     name ='empty_image_filler', 
     version ='0.0.1', 
     author ='Sarah Chen, Esther Cao', 
-    author_email ='insert emails', 
+    author_email ='sarah.chen6@gmail.com, esthercao888@gmail.com', 
     url ='https://github.com/spaceml-org/empty-image-filler', 
-    description ='insert description', 
+    description ='downloading tool to fill missing data gaps', 
     long_description = long_description, 
     long_description_content_type ="text/markdown",  
     packages = find_packages(), 
@@ -20,5 +22,5 @@ setup(
         "Operating System :: OS Independent", 
     ), 
     keywords ='empty_swath_filler emptySwathFiller filler emptySwath swath empty', 
-    install_requires = ["Keras-Preprocessing==1.1.2", "numpy==1.19.4", "tensorflow==2.4.0"]
+    install_requires = requirements
 )
