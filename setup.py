5a773e2d1c5752ff99ef250207e3f68d459c51fc
Sarah Chen <sarah.chen6@gmail.com>
2021-01-12 20:58:50 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = '''Python package used to fill an image's missing data gaps. 
     Specifically useful for obscuring image patterns in a machine learning context.''' 
 
 setup(
-    name ='empty_image_filler', 
+    name ='empty_image_data_filler', 
     version ='0.0.1', 
     author ='Sarah Chen, Esther Cao', 
     author_email ='sarah.chen6@gmail.com, esthercao888@gmail.com', 
@@ -18,7 +18,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent", 
     ), 
-    keywords ='empty_swath_filler emptySwathFiller filler emptySwath swath empty', 
+    keywords ='empty_image_data_filler image_data_filler empty_swath_filler data_filler swath_filler image_filler filler image data swath empty', 
     install_requires = ["Keras-Preprocessing==1.1.2", "numpy==1.19.4", "tensorflow==2.4.0"]
     extras_require = {"dev": ["pytest >= 3.7", ], }
 )
