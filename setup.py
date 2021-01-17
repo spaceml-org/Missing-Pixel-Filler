05ba4034843a305f28ade08102f1f620795a0575
Sarah Chen <sarah.chen6@gmail.com>
2021-01-17 15:06:29 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,20 +7,21 @@ long_description = '''Python package used to fill an image's missing data gaps. 
     Specifically useful for obscuring image patterns in a machine learning context.''' 
 
 setup(
-    name ='missing_pixel_filler', 
-    version ='0.0.1', 
-    author ='Sarah Chen, Esther Cao', 
-    author_email ='sarah.chen6@gmail.com, esthercao888@gmail.com', 
-    url ='https://github.com/spaceml-org/empty-image-filler', 
-    description ='downloading tool to fill missing image data gaps', 
+    name = 'missing_pixel_filler', 
+    version = '0.0.1', 
+    author = 'Sarah Chen, Esther Cao', 
+    author_email = 'sarah.chen6@gmail.com, esthercao888@gmail.com', 
+    url = 'https://github.com/spaceml-org/empty-image-filler', 
+    description = 'downloading tool to fill missing image data gaps', 
     long_description = long_description, 
-    long_description_content_type ="text/markdown",  
+    long_description_content_type = "text/markdown",  
     packages = find_packages(), 
-    classifiers =( 
+    classifiers = ( 
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent", 
     ), 
-    keywords ='missing_pixel_filler missing_image_filler image_data_filler empty_image_filler pixel_filler image_filler filler pixel image', 
-    install_requires = requirements
+    keywords = 'missing_pixel_filler missing_image_filler image_data_filler empty_image_filler pixel_filler image_filler filler pixel image', 
+    install_requires = requirements,
+    py_modules = ['swath_activations']
 )
